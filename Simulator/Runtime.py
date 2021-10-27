2254db82244dd9ba0f9eaaa208594b3ded745781
Kilian Voss <kilian96@live.de>
2021-10-27 18:59:33 +1100

    a copy and paste merge between SimulatorRewrite and master branch, i have no clue of what im doing

--- Simulator/Runtime.py
+++ Simulator/Runtime.py
@@ -2,20 +2,83 @@ import sys
 sys.path.insert(0, '/home/hackerboi/Dokumente/python/TerminalPy/Strategies')
 
 #import modules
-from PriceDiffenrence import PriceDifferencePercentage
+from PriceDiffenrence import PriceDiffPercent
 import SimulatorEngine
+    
+def Sim(Strategy):
+    History = {
+        'AssetPrice': Strategy['AssetValue'],
+        'Cash': SimulatorEngine.Cash,
+        'AssetAmount': SimulatorEngine.Asset,
+        'Trades': []
+    }
 
-def Simulator(Strategy):
+    for index in range(len(Strategy['MAonTop'])):
+        if Strategy['MAonTop'][index] == 5:
+            price = Strategy['AssetValue'][index]
+            SimulatorEngine.buy(price)
+            
+            pnl = PriceDiffPercent(SimulatorEngine.Asset)
+            time = Strategy['time'][index]
+            value = SimulatorEngine.Asset[-1]*price
+            historyObj = {
+                'Time': time,
+                'Direction': 'Long',
+                'Collateral': {
+                    'Type': 'Asset',
+                    'Amount': SimulatorEngine.Asset[-1],
+                    'WorthInCash': value,
+                    'P/L': pnl[-1]
+                }
+            }
 
-    PriceDifference = PriceDifferencePercentage(Strategy['AssetValue'])
+        elif Strategy['MAonTop'][index] == 10:
+            price = Strategy['AssetValue'][index]
+            SimulatorEngine.sell(price)
+            
+            pnl = PriceDiffPercent(SimulatorEngine.Cash)
+            time = Strategy['time'][index]
+            historyObj = {
+                'Time': time,
+                'Direction': 'Short',
+                'Collateral': {
+                    'Type': 'Cash',
+                    'Amount': SimulatorEngine.Cash[-1],
+                    'P/L': pnl[-1]
+                }
+            }
 
-    for element in PriceDifference:
-        #Load Strategy in here and give every output signal a buy or a sell
-        if Strategy['MAonTop'] == 5:
-            SimulatorEngine.buy(element)
-        else:
-            SimulatorEngine.sell(element)
+        History['Trades'].append(historyObj)
+    
+    return(History)
 
-    #Adding the TradeCash key/value Pair 
-    Strategy['TradeCash'] = SimulatorEngine.TradeCash
-    return(Strategy)
+
+
+
+
+
+
+
+
+
+# import sys
+# sys.path.insert(0, '/home/hackerboi/Dokumente/python/TerminalPy/Strategies')
+
+# #import modules
+# from PriceDiffenrence import PriceDifferencePercentage
+# import SimulatorEngine
+
+# def Simulator(Strategy):
+
+#     PriceDifference = PriceDifferencePercentage(Strategy['AssetValue'])
+
+#     for element in PriceDifference:
+#         #Load Strategy in here and give every output signal a buy or a sell
+#         if Strategy['MAonTop'] == 5:
+#             SimulatorEngine.buy(element)
+#         else:
+#             SimulatorEngine.sell(element)
+
+#     #Adding the TradeCash key/value Pair 
+#     Strategy['TradeCash'] = SimulatorEngine.TradeCash
+#     return(Strategy)
--- Simulator/SimulatorEngine.py
+++ Simulator/SimulatorEngine.py
@@ -1,38 +1,35 @@
-TradeCash = [200]
+Cash = [10]
+Asset = [0.1]
 
-def buy(volatility):
-    collateral = TradeCash[-1]
-    VolatilityPercentage = (collateral/100)*(volatility)
-    collateral = collateral + VolatilityPercentage
-    TradeCash.append(collateral)
+def sell(price):
+    collateral = Asset[-1]
+    if collateral == 0:
+        print('Cant sell')
+    else:
+        CashAmount = collateral*price
+        Cash.append(CashAmount)
 
-
-def sell(volatility):
-    collateral = TradeCash[-1]
-    VolatilityPercentage = (collateral/100)*(volatility * (-1))
-    collateral = collateral + VolatilityPercentage
-    TradeCash.append(collateral)
-  
-
-#Zu nem Object Umschreinben
-# class Trade:
-
-#     def __init__(self,Cash):
-#         self.TradeCash = [Cash]
-
-#     def buy(self, volatility):
-#         collateral = self.TradeCash[-1]
-#         VolatilityPercentage = (collateral/100)*(volatility)
-#         collateral = collateral + VolatilityPercentage
-#         self.TradeCash.append(collateral)
-#         print(self.TradeCash)#TradeData['AssetValue'].append
-
-#     def sell(self,volatility):
-#         collateral = self.TradeCash[-1]
-#         VolatilityPercentage = (collateral/100)*(volatility * (-1))
-#         collateral = collateral + VolatilityPercentage
-#         self.TradeCash.append(collateral)
-#         print(self.TradeCash)#TradeData['AssetValue'].append
+def buy(price):
+    collateral = Cash[-1]
+    if collateral == 0:
+        print('Cant buy')
+    else:
+        AssetAmount = collateral/price
+        Asset.append(AssetAmount)
 
 
 
+# TradeCash = [200]
+
+# def buy(volatility):
+#     collateral = TradeCash[-1]
+#     VolatilityPercentage = (collateral/100)*(volatility)
+#     collateral = collateral + VolatilityPercentage
+#     TradeCash.append(collateral)
+
+
+# def sell(volatility):
+#     collateral = TradeCash[-1]
+#     VolatilityPercentage = (collateral/100)*(volatility * (-1))
+#     collateral = collateral + VolatilityPercentage
+#     TradeCash.append(collateral)
