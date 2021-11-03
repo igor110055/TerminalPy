53a3e2c38001e8fa86350f707a998a7515778f1a
Lachlan <lsteele208@gmail.com>
2021-11-03 19:39:36 +1100

    relative path formatting
    added file "find_parent" as first import. This will find the position in the directory tree and allow for relative imports. Must be imported as first module in order to rebase the paths.
    Imports in MAFormatter show the different uses of relative imports that will allow for use when a file from a parent is wanted the import is *from* folder *import* file.
    When a definition is wanted use *from* folder.file *import* definition
    
    To make code more readable you can also use the *as* key:
    *from* folder.file *import* super_long_definintion *as* my_func
    You can then use the key "my_func" for the rest of the file.

--- Strategies/MAFormater.py
+++ Strategies/MAFormater.py
@@ -1,36 +1,35 @@
-import sys
-sys.path.insert(1,'/home/hackerboi/Dokumente/python/TerminalPy/Indicators')
-import Indicator
-from getRidofNan import delNan
+import find_parent
+import Indicators
+from DataIntegrityCheck import DataIntegrityCheck
+from PriceData import PriceDataAsJson
+from Indicators.getRidofNan import delNan
+from PriceData.AverageWDate import AveragePrice
 
-sys.path.insert(2,'/home/hackerboi/Dokumente/python/TerminalPy/PriceData')
+# sys.path.insert(1, '/home/hackerboi/Dokumente/python/TerminalPy/Indicators')
+# sys.path.insert(2, '/home/hackerboi/Dokumente/python/TerminalPy/PriceData')
 # import OHLC_data_CCXT
 # CandleSticks = OHLC_data_CCXT.ohlc
-import PriceDataAsJson
 CandleSticks = PriceDataAsJson.formated
 
-#Import Formated Price Data
-from AverageWDate import AveragePrice
+# Import Formated Price Data
 PriceData = AveragePrice(CandleSticks)
 
-from DataIntegrityCheck import DataIntegrityCheck
 
 sMA5 = Indicator.SMA(PriceData, 5)
 sMA10 = Indicator.SMA(PriceData, 10)
 
+
 def IndicatorsToFormate(dataSet):
     formatedIndicators = []
-    #For Every indicator Dataset we take the firs array and delete the nan Values
-    
+    # For Every indicator Dataset we take the firs array and delete the nan Values
+
     # If the check is passed continue
     DataIntegrityCheck(dataSet)
-    #Delete NaN Values from Indicators
+    # Delete NaN Values from Indicators
     IndicatorDataSetWithoutNaN = delNan(dataSet[0])
-    
-    
 
     Period = dataSet[3]
-    #For Every element in the Second List of the DataSet we delete
+    # For Every element in the Second List of the DataSet we delete
     for element in dataSet[1]:
         if dataSet[1].index(element) < Period:
             temp = dataSet[1]
@@ -38,7 +37,7 @@ def IndicatorsToFormate(dataSet):
             # dataSet[1].remove(element)
             formatedIndicators.append(temp)
 
-    #For Every element in the Second List of the DataSet we delete
+    # For Every element in the Second List of the DataSet we delete
     for element in dataSet[2]:
         if dataSet[2].index(element) <= Period:
             temp = dataSet[2]
@@ -46,11 +45,12 @@ def IndicatorsToFormate(dataSet):
             # dataSet[2].remove(element)
             formatedIndicators.append(temp)
 
-    formatedIndicators.append([IndicatorDataSetWithoutNaN,dataSet[1],dataSet[2],dataSet[3]])
-            
+    formatedIndicators.append([IndicatorDataSetWithoutNaN, dataSet[1], dataSet[2], dataSet[3]])
+
     return formatedIndicators
 
-# Wenn nur eine von beiden ausgeführt wird funzt alles 
+
+# Wenn nur eine von beiden ausgeführt wird funzt alles
 # Werden jedoch beide ausgeführt ist eine immer Falsch
 # Es werden zu wenig Datum und AssetValues gepusht
 # Fehler liegt wahrscheinlich zwischen line 32 und 49
@@ -59,4 +59,3 @@ def IndicatorsToFormate(dataSet):
 # Werden beide ausgeführt failt DataIntegrityCheck immer beim zweiten
 IndicatorsFormated10 = IndicatorsToFormate(sMA10)
 IndicatorsFormated5 = IndicatorsToFormate(sMA5)
-
