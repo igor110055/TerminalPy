1bd5a046ab8cc9bc7e382a43c99d0aa4ebaf891c
Kilian Voss <kilian96@live.de>
2022-01-12 12:15:06 +0800

    New modules fir the Visualisation Api

---
+++ Config/BinanceMetaData.py
@@ -0,0 +1,33 @@
+import requests
+def BinanceMetaData():
+    response = requests.get('https://api.binance.com/api/v3/exchangeInfo')
+    json = response.json()
+
+    Binance = {
+        'Name':'Binance',
+        'AssetPairs':[],
+        'CandleSize' : {
+            'Minute1' : '1m',
+            'Minutes3' : '3m',
+            'Minutes5': '5m',
+            'Minutes15' : '15m',
+            'Minutes30' : '30m',
+            '1Hour': '1h', 
+            '2Hours': '2h',
+            '4Hours' : '4h',
+            '6Hours' : '6h',
+            '8Hours' : '8h',
+            '12Hours' : '12h',
+            '1Day': '1d',
+            '3Days' : '3d',
+            '1Week': '1w',
+            '1Month': '1M'
+        }
+    }
+
+    for element in json['symbols']:
+        Binance['AssetPairs'].append(element['symbol'])
+
+    Binance['AssetPairs'].sort()
+    
+    return Binance
---
+++ Config/DataSourceFrontend.py
@@ -0,0 +1,8 @@
+import find_parent
+from Config.BinanceMetaData import BinanceMetaData
+from Config.MarketStackFetchMetaData import MarketStackData
+
+DataSources = [
+    BinanceMetaData(),
+    # MarketStackData()
+]
