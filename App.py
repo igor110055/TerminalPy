237070499afde548b1f413e7711fa1a824a30f0f
Kilian Voss <kilian96@live.de>
2022-02-27 14:54:38 +0800

    Deleting all unecesary Files and writing Comments to explain how the Backend Simulator works in Detail

--- App.py
+++ App.py
@@ -3,16 +3,22 @@ from Config.Config import PriceData
 
 #Import Indicators (TA-Lib)
 import Indicators.Indicator as Indicator
+# Declaring 2 Indicators with our Bitcoin PriceData from Config
 SMA5 = Indicator.SMA(PriceData, 5)
 SMA10 = Indicator.SMA(PriceData, 10)
 
 #Import Strategies
 from Strategies.MACrossings import SMAtoSMACompare
+# This is a module which compares 2 Moving Averages to each other
+# with this output the Simulator module will be able to detect 
+# when the Moving Average lines cross each other
 SMA5vs10 = SMAtoSMACompare(SMA10 , SMA5)
 
 #Import Simulator
 from Simulator.Runtime import Simulator
 Simulation = Simulator(SMA5vs10)
+# The Output from the Simulater Module needs to be analyzed to 
+# tell us if we would have gained or losst money executing that Strategy
 from Simulator.RuntimeOutputFormater import Formater
 FormatedSimulation = Formater(Simulation)
 
--- Simulator/SimulatorEngine.py
+++ Simulator/SimulatorEngine.py
@@ -1,3 +1,6 @@
+# So in this Case before we start trading, imagine we have a Brokerage Account
+# with no Cash, but with only 0.1 Bitcoin as our Ballange to start with
+
 # List Cannot be empty, if no Asset or no Cash just insert 0
 Cash = [0]
 Asset = [0.01]
@@ -5,6 +8,10 @@ Asset = [0.01]
 def sell(price):
     collateral = Asset[-1]
     if collateral == 0:
+        # If the first Trade that our Bot would make would be a Sell we could not 
+        # execute that if we would not have Bitcoin in our Account
+        # But in our Scenrio thats not going to happen
+
         print('Cant sell')
     else:
         CashAmount = round(collateral*price, 2)
@@ -13,6 +20,8 @@ def sell(price):
 def buy(price):
     collateral = Cash[-1]
     if collateral == 0:
+        # If the first Trade that our Bot would make would be a buy we obviously cannot 
+        # execute that since we have no Cash in our Account
         print('Cant buy')
     else:
         AssetAmount = round(collateral/price, 8)
