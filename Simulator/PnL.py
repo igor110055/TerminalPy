d1095a8fa09c685f8122dab7653b3458871f350f
Kilian Voss <kilian96@live.de>
2022-04-30 14:56:53 +0800

    new file to source out pnl logic

---
+++ Simulator/PnL.py
@@ -0,0 +1,8 @@
+# def PnL(trade):
+#     if trade['Direction'] == 'Long':
+#         self.unformated_history['Trades'][index]['PnL'] = round(asset_price_change_percentage, 2)
+#     elif trade['Direction'] == 'Short':
+#     invert_PnL = asset_price_change_percentage
+#         self.unformated_history['Trades'][index]['PnL'] = round( invert_PnL, 2)
+
+            
