f3128f3c3446923890262b1d48ab45405e4c372e
Kilian Voss <kilian96@live.de>
2022-05-07 15:28:27 +1000

    working irovrhbe

--- Simulator/LiquidationPrices.py
+++ Simulator/LiquidationPrices.py
@@ -7,18 +7,10 @@
 
 def Binance(trade, price_data):
     
-    position_value_cash_equivalent = trade
-    # portfolio_value_cash_equivalent =
-    asset_price_position = trade['AssetPrice']
-    leverage = trade['Leverage']
-    direction = trade['Direction']
-    # calculate knockout price
-
-
     # Calculate knock out price 
     # take date
     # take position size to calculate Margin
-    # take leverage faktor
+    # take leverage leverage_factor
     # liquidation_price(self.unformated_history['Trades'][index])
     # google how knock out price is calculated
 
@@ -30,6 +22,43 @@ def Binance(trade, price_data):
         #       return false
         #   elif price == knockout price
         #        return price, pricedata['date'][index]
+
+
+    # position_value_cash_equivalent = trade[]
+    # portfolio_value_cash_equivalent = trade[]
+    asset_price_position_entry = trade['AssetPrice']
+    leverage_factor = trade['Leverage']
+    direction = trade['Direction']
+    # calculate knockout price
+
+
+
+
+    price = 0
+    tradecollateral = 400
+    accountballance = 450
+   
+    
+    schwankungAbsolut = price - asset_price_position_entry
+    schwankung = ((price / asset_price_position_entry) - 1) * 100
+
+    leverage = tradecollateral * leverage_factor
+
+    profitloss = 0
+
+    liquidationlevel = 0
+
+    if direction == 'Long':
+        profitloss = (leverage / 100) * schwankung
+        liquidationlevel = ((0.8 * tradecollateral) - accountballance) / (leverage / 100)
+    elif direction == 'Short':
+        profitloss = (leverage / 100) * (-schwankung)
+        liquidationlevel = (-((0.8 * tradecollateral) - accountballance) / (leverage / 100))
+
+
+    equity = accountballance + profitloss
+    marginlevel = (equity / tradecollateral) * 100
+    liquidationpreis = asset_price_position_entry-((-liquidationlevel * 0.01)*asset_price_position_entry)
     pass
 
 def Kraken(self):
